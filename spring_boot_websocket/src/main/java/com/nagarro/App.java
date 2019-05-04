195abc6ea7e01ccdcefc7f9a4c2a55a8df185e30
vineet7838 <vineetagarwal267@gmail.com>
2019-05-04 11:56:26 +0530

    Init

---
+++ spring_boot_websocket/src/main/java/com/nagarro/App.java
@@ -0,0 +1,13 @@
+package com.nagarro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
